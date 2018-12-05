1e34aa3f75e2f5bbda2631791e4540f853f330ee
lyoshakarpenko <lyoshakarpenko@yandex.ru>
2018-12-05 08:55:47 +0300

    Add files via upload

---
+++ mytomcat/attributes/default.rb
@@ -0,0 +1,10 @@
+# Current version
+default['version'] = '1.0.3'
+#Image tag
+default['flag']='0'
+#Url registry
+default['url'] = '192.168.0.111:5000'
+# Path to share folder
+default['file_path']='/vagrant/test.txt'
+# Path to swarm certificates 
+default['certificates_path'] = '/var/lib/docker/swarm/certificates/swarm-node.key'
