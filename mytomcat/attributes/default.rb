df2898cfbb132a7b138815e73a6e07b2a5fc090f
lyoshakarpenko <lyoshakarpenko@yandex.ru>
2018-12-05 17:52:31 +0300

    Update default.rb

--- mytomcat/attributes/default.rb
+++ mytomcat/attributes/default.rb
@@ -1,10 +1,4 @@
 # Current version
 default['version']='1.0.5'
-#Image tag
-default['flag']='0'
 #Url registry
 default['url'] = '192.168.0.111:5000'
-# Path to share folder
-default['file_path']='/vagrant/test.txt'
-# Path to swarm certificates 
-default['certificates_path'] = '/var/lib/docker/swarm/certificates/swarm-node.key'
