358c84c901a3a737802e703c8fc216f5016f9cd4
lyoshakarpenko <lyoshakarpenko@yandex.ru>
2018-12-05 18:50:08 +0300

    Update default.rb

--- mytomcat/recipes/default.rb
+++ mytomcat/recipes/default.rb
@@ -11,26 +11,27 @@ docker_image "#{node['url']}/mytomcat" do
     tag node['version']
     action :pull
 end
+
 ENV['flag']='0'
 ENV['flag']=`docker ps -a | grep '8080->' -c`
 puts "#{ENV['flag'][0]}" 
 case ENV['flag'][0]
-	when '0'
-	docker_container 'mytomcat_blue' do
-		repo "#{node['url']}/mytomcat"
-		tag node['version']
-		port '8080:8080'
-	end	
-	docker_container 'mytomcat_green' do
-		action [:kill, :delete]
-	end
-	else
-	docker_container 'mytomcat_green' do
-		repo "#{node['url']}/mytomcat"
-		tag node['version']
-		port '8081:8081'
-	end
-	docker_container 'mytomcat_blue' do
-		action [:kill, :delete]
-	end
+    when '0'
+    docker_container 'mytomcat_blue' do
+        repo "#{node['url']}/mytomcat"
+        tag node['version']
+        port '8080:8080'
+    end	
+    docker_container 'mytomcat_green' do
+        action [:kill, :delete]
+    end
+    else
+    docker_container 'mytomcat_green' do
+        repo "#{node['url']}/mytomcat"
+        tag node['version']
+        port '8081:8081'
+    end
+    docker_container 'mytomcat_blue' do
+        action [:kill, :delete]
+    end
 end
