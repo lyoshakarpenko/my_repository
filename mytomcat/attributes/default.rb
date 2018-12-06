8bf9ad4eb36f523a006b7979563d792e1f1f12d9
lyoshakarpenko <lyoshakarpenko@yandex.ru>
2018-12-06 09:00:08 +0000

    change version to 1.0.2

--- mytomcat/attributes/default.rb
+++ mytomcat/attributes/default.rb
@@ -1,4 +1,4 @@
 # Current version
-default['mytomcat']['version']='1.0.6'
+default['mytomcat']['version']='1.0.2'
 #Url registry
 default['mytomcat']['url'] = '192.168.0.111:5000'
--- mytomcat/metadata.rb
+++ mytomcat/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mytomcat'
 long_description 'Installs/Configures mytomcat'
-version '1.0.6'
+version '1.0.2'
 chef_version '>= 13.0'
 depends 'docker'
 
